689650794e9159bc9a432c5087743a2116dde596
Hunain Parekh <hunain.parekh@xloopdigital.com>
2023-02-22 21:52:56 +0500

    routing issue resolved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter, Routes,Route } from "react-router-dom";
 import Loader from "./pages/Loader/Loader";
 import Main from "./pages/Main/Main";
-
+import Cart from "./pages/Cart/Cart";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import ProductList from "./pages/ProductList/ProductList";
+import Thanks from "./pages/Thanks/Thanks";
 
 function App() {
   const [loader, setLoader] = useState(true);
@@ -12,7 +16,18 @@ function App() {
   });
   return (
     <div className="App">
-      {loader ? <Loader /> : <Main />}
+      {loader ? <Loader /> :
+      <BrowserRouter>
+        <Routes>
+          <Route to="/" element={<Main />}>
+                <Route index element={<ProductList/>}></Route>
+                <Route path="/details/:productID" element={<ProductDetails/>}></Route>
+                <Route path="/cart" element={<Cart/>}></Route>
+          </Route>
+          <Route path="/thanks" element={<Thanks/>}></Route>
+        </Routes>
+      </BrowserRouter>
+      }
     </div>
   );
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,22 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
-import Cart from "../Cart/Cart";
-import ProductDetails from "../ProductDetails/ProductDetails";
-import ProductList from "../ProductList/ProductList";
-import Thanks from "../Thanks/Thanks";
+
 
 const Main = ()=>{
     return(
         <>
             <Navbar/>
-            <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<ProductList/>}></Route>
-                <Route path="/details/:productID" element={<ProductDetails/>}></Route>
-                <Route path="/cart" element={<Cart/>}></Route>
-                <Route path="/thanks" element={<Thanks/>}></Route>
-            </Routes>
-            </BrowserRouter>
+            <Outlet/>
         </>
     );
 }
