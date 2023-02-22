26974668d3b18c7617804d2a754c496fe4894420
Hunain Parekh <hunain.parekh@xloopdigital.com>
2023-02-22 21:55:25 +0500

    routing done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,22 @@
-import { Icon } from '@iconify/react';
-import './navbar.css';
-import logo from '../img/logo.png';
+import { Icon } from "@iconify/react";
+import "./navbar.css";
+import logo from "../img/logo.png";
+import { Link } from "react-router-dom";
 
-const Navbar = ()=>{
-    return(
-        <nav className='navbar'>
-            <div className="logo-div">
-                <img src={logo} alt='logo'/>
-            </div>
-            <div className="cart-icon-div">
-                <Icon icon="material-symbols:shopping-cart" className='cart-icon'/>
-            </div>
-        </nav>
-    );
-}
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo-div">
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+      </div>
+      <div className="cart-icon-div">
+        <Link to="cart">
+          <Icon icon="material-symbols:shopping-cart" className="cart-icon" />
+        </Link>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
